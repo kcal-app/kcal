ebc6f9cdd5bd9dc4727b8dc0a3d5f1448b029061
Christopher C. Wells <git@chris-wells.net>
2020-12-31 15:00:38 -0800

    Remove unused JSON:API elements

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-JsonApi::register('default')->routes(function ($api) {
-    $api->resource('food-amounts')->relationships(function ($relations) {
-        $relations->hasOne('food');
-        $relations->hasOne('recipe');
-    });
-    $api->resource('foods');
-    $api->resource('recipes')->relationships(function ($relations) {
-        $relations->hasMany('food-amounts');
-        $relations->hasOne('steps');
-    });
-    $api->resource('recipe-steps')->relationships(function ($relations) {
-        $relations->hasOne('recipe');
-    });
-});
