80bdd0ebae4298bd92a0cfcafc445f59f02a148b
Christopher C. Wells <git@chris-wells.net>
2021-01-23 16:29:15 -0800

    Set default API in boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use CloudCreativity\LaravelJsonApi\LaravelJsonApi;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        LaravelJsonApi::defaultApi('v1');
     }
 }
