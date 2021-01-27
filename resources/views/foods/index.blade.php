bc77f923a2a244daf17bd47f5e2a7e3b73976e59
Christopher C. Wells <git@chris-wells.net>
2021-01-27 05:57:57 -0800

    Remove edit links from index views

--- resources/views/foods/index.blade.php
+++ resources/views/foods/index.blade.php
@@ -19,15 +19,13 @@
                         <x-slot name="results">
                             <template x-for="food in results" :key="food">
                                 <div class="p-2 font-light rounded-t border-2 border-gray-400">
-                                    <a class="h-6 w-6 text-gray-500 hover:text-gray-700 hover:border-gray-300 float-right text-sm"
-                                       x-bind:href="food.editUrl">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                        </svg>
-                                    </a>
-                                    <div class="text-2xl">
-                                        <span x-text="food.name"></span><span class="text-gray-500" x-text="`, ${food.detail}`" x-show="food.detail"></span>
+                                    <div class="flex justify-between items-baseline">
+                                        <div class="text-2xl">
+                                            <a x-bind:href="food.showUrl"
+                                               class="text-gray-600 hover:text-gray-800">
+                                                <span x-text="food.name"></span><span class="text-gray-500" x-text="`, ${food.detail}`" x-show="food.detail"></span>
+                                            </a>
+                                        </div>
                                     </div>
                                     <div class="text-xl text-gray-600" x-text="food.brand" x-show="food.brand"></div>
                                     <div class="font-bold">
