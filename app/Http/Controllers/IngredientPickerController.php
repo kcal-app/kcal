a7d89290b8381689ea405b80e7411787f7b82d66
Christopher C. Wells <git@chris-wells.net>
2021-02-24 18:42:56 -0800

    Sort combines ingredient picker results

--- app/Http/Controllers/IngredientPickerController.php
+++ app/Http/Controllers/IngredientPickerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Food;
 use App\Models\Recipe;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -14,12 +15,12 @@ class IngredientPickerController extends Controller
      */
     public function search(Request $request): JsonResponse
     {
-        $results = [];
+        $results = new Collection();
         $term = $request->query->get('term');
         if (!empty($term)) {
-            $results = Food::search($term)->get();
+            $results = $results->merge(Food::search($term)->get());
             $results = $results->merge(Recipe::search($term)->get());
         }
-        return response()->json($results);
+        return response()->json($results->sortBy('name')->values());
     }
 }
