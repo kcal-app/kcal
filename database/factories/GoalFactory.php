c67aa89555b0dfefab7d29c86d198c11f735fd76
Christopher C. Wells <git@chris-wells.net>
2021-03-29 21:27:12 -0700

    Remove hardcoded create statements in factories (WIP)

--- database/factories/GoalFactory.php
+++ database/factories/GoalFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\Goal;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class GoalFactory extends Factory
@@ -18,8 +17,6 @@ class GoalFactory extends Factory
      */
     public function definition()
     {
-        /** @var \App\Models\User $user */
-        $user = User::factory()->create();
         $from = $this->faker->dateTimeThisMonth;
         $to = $this->faker->dateTimeBetween($from, '+1 year');
         return [
@@ -28,19 +25,6 @@ class GoalFactory extends Factory
             'frequency' => $this->faker->randomElement(Goal::$frequencyOptions)['value'],
             'name' => $this->faker->randomElement(Goal::getNameOptions())['value'],
             'goal' => $this->faker->numberBetween(0, 2000),
-            'user_id' => $user->id,
         ];
-    }
-
-    /**
-     * Define a specific user.
-     */
-    public function user(User $user): static
-    {
-        return $this->state(function (array $attributes) use ($user) {
-            return [
-                'user_id' => $user->id,
-            ];
-        });
     }
 }
--- database/factories/RecipeStepFactory.php
+++ database/factories/RecipeStepFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Recipe;
 use App\Models\RecipeStep;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -18,24 +17,9 @@ class RecipeStepFactory extends Factory
      */
     public function definition(): array
     {
-        /** @var \App\Models\Recipe $recipe */
-        $recipe = Recipe::factory()->create();
         return [
-            'recipe_id' => $recipe->id,
             'number' => $this->faker->numberBetween(1, 50),
             'step' => $this->faker->realText(500),
         ];
-    }
-
-    /**
-     * Define a specific recipe.
-     */
-    public function recipe(Recipe $recipe): static
-    {
-        return $this->state(function (array $attributes) use ($recipe) {
-            return [
-                'recipe_id' => $recipe->id,
-            ];
-        });
     }
 }
