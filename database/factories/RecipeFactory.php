1213a93392fa755f1cea2f7c7aa82b03b2c65b65
Christopher C. Wells <git@chris-wells.net>
2021-04-02 23:34:23 -0700

    Add Media API tests

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\Recipe;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class RecipeFactory extends Factory
 {
@@ -29,5 +31,21 @@ class RecipeFactory extends Factory
             'weight' => $this->faker->randomFloat(1, 60, 2000),
             'tags' => $this->faker->words,
         ];
+    }
+
+    /**
+     * Create a recipe and add fake media to it.
+     */
+    public function createOneWithMedia(array $attributes = []): Recipe {
+        Storage::fake('tests');
+        /** @var \App\Models\Recipe $recipe */
+        $recipe = $this->createOne($attributes);
+        $file = UploadedFile::fake()->image('recipe.jpg', 1600, 900);
+        $path = $file->store('tests');
+        $recipe->addMediaFromDisk($path)
+            ->usingName($recipe->name)
+            ->usingFileName("{$recipe->slug}.jpg")
+            ->toMediaCollection();
+        return $recipe;
     }
 }
