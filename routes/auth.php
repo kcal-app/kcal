d18247e68dafd9006942bac01028e81814b4b09f
Christopher C. Wells <git@chris-wells.net>
2021-04-20 15:21:36 -0700

    Reorganize routes in to middleware groups

--- routes/auth.php
+++ routes/auth.php
@@ -1,15 +1,39 @@
 <?php
 
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\GoalController;
+use App\Http\Controllers\IngredientPickerController;
+use App\Http\Controllers\JournalEntryController;
+use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-                ->middleware('guest')
-                ->name('login');
+/*
+|--------------------------------------------------------------------------
+| Authorized User Routes
+|--------------------------------------------------------------------------
+*/
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest');
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    // Foods.
+    Route::resource('foods', FoodController::class);
+    Route::get('/foods/{food}/delete', [FoodController::class, 'delete'])->name('foods.delete');
 
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
-                ->name('logout');
+    // Goals.
+    Route::resource('goals', GoalController::class);
+    Route::get('/goals/{goal}/delete', [GoalController::class, 'delete'])->name('goals.delete');
+
+    // Ingredient picker.
+    Route::get('/ingredient-picker/search', [IngredientPickerController::class, 'search'])->name('ingredient-picker.search');
+
+    // Journal entries.
+    Route::get('/journal-entries/create/from-nutrients', [JournalEntryController::class, 'createFromNutrients'])->name('journal-entries.create.from-nutrients');
+    Route::post('/journal-entries/create/from-nutrients', [JournalEntryController::class, 'storeFromNutrients'])->name('journal-entries.store.from-nutrients');
+    Route::resource('journal-entries', JournalEntryController::class);
+    Route::get('/journal-entries/{journal_entry}/delete', [JournalEntryController::class, 'delete'])->name('journal-entries.delete');
+
+    // Recipes.
+    Route::resource('recipes', RecipeController::class);
+    Route::get('/recipes/{recipe}/delete', [RecipeController::class, 'delete'])->name('recipes.delete');
+});
