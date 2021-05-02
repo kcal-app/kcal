3ae6c6ef13a50d2cd1d1a20ae028094968e5e36e
Christopher C. Wells <git@chris-wells.net>
2021-05-02 09:41:20 -0700

    Add remaining fields for Goal edit form

--- resources/views/goals/edit.blade.php
+++ resources/views/goals/edit.blade.php
@@ -41,17 +41,34 @@
                                     :hasError="$errors->has('to')" />
                 </div>
 
-                <!-- Goal -->
-                <div class="flex-auto">
-                    <x-inputs.label for="goal" value="Goal" />
-                    <x-inputs.input name="goal"
-                                    type="number"
-                                    step="any"
-                                    class="block w-full"
-                                    :value="old('goal', $goal->goal)"
-                                    :hasError="$errors->has('goal')"
-                                    required />
-                </div>
+                <!-- Days of the week -->
+                <fieldset class="flex-auto">
+                    <legend class="block font-medium text-sm text-gray-700">Days of the week</legend>
+                    <div class="inline-flex justify-between divide-x border rounded-md shadow-sm border-gray-300">
+                        @foreach(\App\Models\Goal::days() as $day)
+                            <x-inputs.label class="inline-flex items-center p-2">
+                                <x-inputs.input type="checkbox" name="days[]" :value="$day['value']" />
+                                <span class="ml-2">{{ \Illuminate\Support\Str::ucfirst($day['label']) }}</span>
+                            </x-inputs.label>
+                        @endforeach
+                    </div>
+                </fieldset>
+            </div>
+            <div class="flex flex-col space-y-4 md:flex-row md:space-y-0">
+            @foreach (\App\Support\Nutrients::all()->sortBy('weight') as $nutrient)
+                <!-- {{ ucfirst($nutrient['value']) }} -->
+                    <div class="flex-auto">
+                        <x-inputs.label for="{{ $nutrient['value'] }}"
+                                        :value="ucfirst($nutrient['value']) . ($nutrient['unit'] ? ' (' . $nutrient['unit'] . ')' : '')"/>
+
+                        <x-inputs.input name="{{ $nutrient['value'] }}"
+                                        type="number"
+                                        step="any"
+                                        class="block w-full mt-1 md:w-5/6"
+                                        :value="old($nutrient['value'], $goal->{$nutrient['value']})"
+                                        :hasError="$errors->has($nutrient['value'])"/>
+                    </div>
+                @endforeach
             </div>
         </div>
 
