cf51670727a509e9a016d94f8d844a70a55f49c0
Christopher C. Wells <git@chris-wells.net>
2021-09-27 09:02:44 -0700

    Add CSP policy to all responses

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,6 +31,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Spatie\Csp\AddCspHeaders::class,
         ],
 
         'api' => [
