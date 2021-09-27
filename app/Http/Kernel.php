419fcc2cb93cc24649dad1f09145c765b17549b6
Christopher C. Wells <git@chris-wells.net>
2021-09-27 10:02:11 -0700

    Disable browser cache with Cache-Control

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Spatie\Csp\AddCspHeaders::class,
+            \App\Http\Middleware\DisableBrowserCache::class,
         ],
 
         'api' => [
