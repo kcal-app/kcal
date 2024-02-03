9aea9114f76524f5d8b5dd80702b0cc79151e830
Christopher C. Wells <git-2022@chris-wells.net>
2024-02-03 08:02:51 -0800

    Col flex recipe actions for all sizes

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -178,7 +178,7 @@
                     </div>
                 </div>
             </div>
-            <section class="flex flex-row space-x-2 justify-around md:flex-col md:space-y-2 md:space-x-0">
+            <section class="flex flex-col space-y-2">
                 <x-log-journalable :journalable="$recipe"></x-log-journalable>
                 <x-button-link.gray href="{{ route('recipes.edit', $recipe) }}">
                     Edit Recipe
